234febc2e935132db1bd407bf3728dd7b767f550
DeanAK1987 <deanak@uw.edu>
2023-06-05 20:46:29 -0700

    exam stuff

--- HybridSystemForFinal.py
+++ HybridSystemForFinal.py
@@ -17,4 +17,8 @@ def find_generators(n):
         if all(pow(i, j, n) != 1 for j in range(2, n)):
             generators.append(i)
     return generators
-
+g=8
+h=g**4
+r=3
+c1 = g**r
+c = pow(c1, h**4)
